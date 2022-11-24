9b41ce64f7d72d6b0c6f348b69ad5bd350e15d88
Edoardo Grassi <edo.g@hotmail.it>
2022-11-24 12:46:50 +0100

    chore: version 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mailupy',
-    version='1.1.0',
+    version='1.1.1',
     url='https://github.com/lotrekagency/mailupy',
     install_requires=[
         "requests==2.24.0"
